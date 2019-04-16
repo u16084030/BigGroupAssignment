2f936d0785ca428e2d916d9db20250fc60417f3f
Gaby <u16084030@tuks.co.za>
2019-04-16 16:02:31 +0200

    Inserts and API started

---
+++ Diseases_API/Diseases_API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Diseases_API.Startup))]
+
+namespace Diseases_API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
